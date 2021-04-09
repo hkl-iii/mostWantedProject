9f9771271feb71b4e81823a7867064f055ca1cc4
RayHubb <hubbard_raimon@yahoo.com>
2021-04-09 03:05:47 -0500

    Fixed the data table, added criteria to the form, and fixed some syntax errors

--- app.js
+++ app.js
@@ -7,18 +7,17 @@ function searchByName(){
 
     // "people" is coming from the data.js file. We have access to it within this JavaScript file.
     let filteredPeople = people.filter(function (person) {
-        if(person.firstName === firstNameInput && person.lastName === lastNameInput){
+        if(person.firstName === firstNameInput || person.lastName === lastNameInput){
             return true;
         }
-            return false;
-            
+            return false;            
         });
-    searchByName();
+
     
     // Rather than console logging, you need to append the filteredPeople to a table.
     if(filteredPeople.length > 0){
         console.log(filteredPeople);
     }else{
-        console.log('Sorry, looks like there is no one with that name.');
+        alert('Sorry, looks like there is no one who matches that criteria.');
     }
 }
