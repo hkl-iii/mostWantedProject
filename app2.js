f12cf02c7ff7ca3b26a2e43c7e0507d77ddfee8d
unknown <rahleggon@gmail.com>
2021-04-10 23:36:34 -0500

    changed titles

--- app2.js
+++ app2.js
@@ -6,7 +6,7 @@ search_input.addEventListener("keyup", function(e){
   let span_items = document.querySelectorAll(".table_body .name span");
   let table_body = document.querySelector(".table_body ul");
   let search_item = e.target.value.toLowerCase();
-    console.log(e.target.value);
+  
  
  span_items.forEach(function(item){
    if(item.textContent.toLowerCase().indexOf(search_item) != -1){
