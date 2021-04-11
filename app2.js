9599ebe46c0ee35fbdd0b0f08c38760074e4516a
unknown <rahleggon@gmail.com>
2021-04-10 20:26:12 -0500

    started totally different table with new html, css, & js. New table still incomplete in its css.

---
+++ app2.js
@@ -0,0 +1,20 @@
+"use strict";
+
+var search_input = document.querySelector("#search_input");
+
+search_input.addEventListener("keyup", function(e){
+  let span_items = document.querySelectorAll(".table_body .name span");
+  let table_body = document.querySelector(".table_body ul");
+  let search_item = e.target.value.toLowerCase();
+    console.log(e.target.value);
+ 
+ span_items.forEach(function(item){
+   if(item.textContent.toLowerCase().indexOf(search_item) != -1){
+      item.closest("li").style.display = "block";
+   }
+   else{
+     item.closest("li").style.display = "none";
+     }
+ })
+  
+});
